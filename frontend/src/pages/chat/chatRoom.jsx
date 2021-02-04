64a7dd150b4584e7577b839557f97af4b95b7b9c
aviral243 <aviralgangwar24@gmail.com>
2021-02-04 17:24:55 +0530

    css tweaking

--- frontend/src/pages/chat/chatRoom.jsx
+++ frontend/src/pages/chat/chatRoom.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import Header from "components/header/header";
+import Sidebar from "components/sidebar/sidebar";
 
 import "./chatRoom.css";
 import useChat from "./useChat";
@@ -19,31 +21,35 @@ const ChatRoom = (props) => {
   };
 
   return (
-    <div className="chat-room-container">
-      <h1 className="room-name">Room: {roomId}</h1>
-      <div className="messages-container">
-        <ol className="messages-list">
-          {messages.map((message, i) => (
-            <li
-              key={i}
-              className={`message-item ${
-                message.ownedByCurrentUser ? "my-message" : "received-message"
-              }`}
-            >
-              {message.body}
-            </li>
-          ))}
-        </ol>
+    <div>
+      <Header />
+      <Sidebar />
+      <div className="chat-room-container">
+        <h1 className="room-name">Room: {roomId}</h1>
+        <div className="messages-container">
+          <ol className="messages-list">
+            {messages.map((message, i) => (
+              <li
+                key={i}
+                className={`message-item ${
+                  message.ownedByCurrentUser ? "my-message" : "received-message"
+                }`}
+              >
+                {message.body}
+              </li>
+            ))}
+          </ol>
+        </div>
+        <textarea
+          value={newMessage}
+          onChange={handleNewMessageChange}
+          placeholder="Write message..."
+          className="new-message-input-field"
+        />
+        <button onClick={handleSendMessage} className="send-message-button">
+          Send
+        </button>
       </div>
-      <textarea
-        value={newMessage}
-        onChange={handleNewMessageChange}
-        placeholder="Write message..."
-        className="new-message-input-field"
-      />
-      <button onClick={handleSendMessage} className="send-message-button">
-        Send
-      </button>
     </div>
   );
 };
