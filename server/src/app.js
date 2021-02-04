2ab59bae248263c1fe59a3617833d6cc15f953f7
aviral243 <aviralgangwar24@gmail.com>
2021-02-04 05:48:15 +0530

    establish websocket connection

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,25 @@
-var logger = require('morgan');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var indexRouter = require('./routes/index');
-var app = express();
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use('/v1', indexRouter);
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+        origin: "*",
+    }
+});
 
+const STATIC_CHANNELS = ['global_notifications', 'global_chat']
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
+
+io.on('connection', (socket) => {
+    console.log('new client')
+    socket.emit('connection', null)
+})
 module.exports = app;
